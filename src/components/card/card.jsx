d1c6db4681f3cef326d59293abf9234428a77bfd
Joshua Lynch <Joshlynch2001@gmail.com>
2025-12-14 17:24:34 -0400

    added stuff

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,9 @@
 import "./card.css"
-function Card() {
+function Card({title, desc}) {
   return (
     <div className="card">
-      <div className="card-title">Title</div>
-      <div className="card-content">This is the content of the card</div>
+      <div className="card-title">{title}</div>
+      <div className="card-content">{desc}</div>
     </div>
   );
 }
--- src/pages/adminGame.jsx
+++ src/pages/adminGame.jsx
@@ -2,14 +2,21 @@ import Card from "../components/card/card"
 import "./adminGame.css"
 function AdminGame() {
     return (
-        <div className="page-content">
-            <div className="card-section">
-                <Card/>
-            </div>
-            <div className="player-container">
-
-            </div>
+      <div className="page-content">
+        <div className="card-section">
+          <Card title={"New Title"} desc={"This is the new desc"} />
         </div>
-    )
+        <div className="details-container">
+          <div className="stats-container">
+            <div className="card-title">Round 1</div>
+            <div className="bubble-content">
+              <div>Most Drinks: Josh</div>
+              <div>Most Answers: Edilyn</div>
+            </div>
+          </div>
+          <div className="player-container"></div>
+        </div>
+      </div>
+    );
 }
 export default AdminGame
