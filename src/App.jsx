561597ac82a9390175f99c280faf3f6f9c7eebdc
Joshua Lynch <Joshlynch2001@gmail.com>
2025-12-15 01:55:10 -0400

    added all css to their own page css folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar/navbar.jsx';
 import AdminGame from './pages/adminGame.jsx';
-import AdminLobby from './pages/adminLobby.jsx';
-import GeneralPlayer from './pages/generalPlayer.jsx';
-import Lobby from './pages/lobby.jsx';
-import SelectedPlayer from './pages/selectedPlayer.jsx';
 
 function App() {
 
   return (
     <>
     <Navbar/>
-    <GeneralPlayer/>
+    <AdminGame/>
     </>
   )
 }
