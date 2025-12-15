7e271602143dba6f5301f974c40278cd6fa15e50
Joshua Lynch <Joshlynch2001@gmail.com>
2025-12-15 02:13:00 -0400

    seeing how they all look together

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css';
 import Navbar from './components/navbar/navbar.jsx';
-import SelectedPlayer from "./pages/selectedPlayer.jsx"
+import SelectedPlayer from "./pages/selectedPlayer.jsx";
+import AdminGame from "./pages/adminGame.jsx";
+import AdminLobby from "./pages/adminLobby.jsx";
+import GeneralPlayer from './pages/generalPlayer.jsx';
+import Lobby from "./pages/lobby.jsx";
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <AdminGame/>
+    <AdminLobby/>
+    <GeneralPlayer/>
+    <Lobby/>
     <SelectedPlayer/>
     </>
   )
