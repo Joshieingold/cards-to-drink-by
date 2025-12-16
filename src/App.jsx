bf86044c5e9d32432d5de481efc0c8ed1f21137d
Joshua Lynch <Joshlynch2001@gmail.com>
2025-12-16 00:43:32 -0400

    made if else logic based on client state

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,60 @@ import AdminGame from "./pages/adminGame.jsx";
 import AdminLobby from "./pages/adminLobby.jsx";
 import GeneralPlayer from './pages/generalPlayer.jsx';
 import Lobby from "./pages/lobby.jsx";
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState("Admin");
+  const [gameState, setGameState] = useState("Game");
+  const [selectedUser, setSelectedUser] = useState(false);
+  if (gameState == "Lobby") {
+    if (user == "Admin") {
+      return (
+        <>
+        <Navbar/>
+        <AdminLobby/>
+       </>
+      )
+    }
+    else {
 
-  return (
-    <>
-    <Navbar/>
-    <AdminGame/>
-    <AdminLobby/>
-    <GeneralPlayer/>
-    <Lobby/>
-    <SelectedPlayer/>
-    </>
-  )
+      return (
+        <>
+        <Navbar/>
+        <Lobby/>
+       </>
+      )
+    }
+  }
+  else {
+    if (user == "Admin") {
+      return (
+        <>
+        <Navbar/>
+        <AdminGame/>
+       </>
+      )
+    }
+    else {
+      if (selectedUser) {
+        return (
+        <>
+          <Navbar/>
+          <SelectedPlayer/>
+        </>
+        )
+      }
+      else {
+        return (
+
+        <>
+          <Navbar/>
+          <GeneralPlayer/>
+        </>
+        )
+      }
+    }
+  }
 }
 
 export default App
